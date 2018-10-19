d33cdcf35ddca7be5a7285f34619c8c3764514ee
Gary Chisholm <garyjchisholm@gmail.com>
2018-10-19 17:45:27 +1100

    Cleaning it up

--- main.jsx
+++ main.jsx
@@ -1,104 +1,126 @@
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>EASY HIIT</h1>
-        <div className="container">
-          <Timer />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+const defaultTime = 60;
+const Time = React.createContext({
+  seconds: defaultTime,
+  onStart: () => {},
+  onStop: () => {},
+  onReset: () => {},
+  isCountingDown: false
+});
 
-class Motivation extends React.Component {
+const TimeView = () => (
+  <Time.Consumer>
+    {({ seconds }) => (
+      <div className="time">
+        {seconds < 10 ? "0" : ""}
+        {seconds}
+        <small>seconds</small>
+      </div>
+    )}
+  </Time.Consumer>
+);
+
+const StartButton = () => (
+  <Time.Consumer>
+    {({ onStart, isCountingDown }) => (
+      <button
+        onClick={isCountingDown ? () => {} : onStart}
+        disabled={isCountingDown}
+      >
+        start
+      </button>
+    )}
+  </Time.Consumer>
+);
+
+const StopButton = () => (
+  <Time.Consumer>
+    {({ onStop, isCountingDown }) => (
+      <button
+        onClick={!isCountingDown ? () => {} : onStop}
+        disabled={!isCountingDown}
+      >
+        stop
+      </button>
+    )}
+  </Time.Consumer>
+);
+
+const ResetButton = () => (
+  <Time.Consumer>
+    {({ onReset, isCountingDown }) =>
+      isCountingDown && <button onClick={onReset}>reset</button>
+    }
+  </Time.Consumer>
+);
+
+class Countdown extends React.Component {
   constructor(props) {
     super(props);
-    this.text = [
-      "Go for it!",
-      "Don't stop!",
-      "Feel the burn!",
-      "100%",
-      "No pain, no gain",
-      "Great job",
-      "Keep it up",
-      "Sweat through it"
-    ];
+    this.initialState = {
+      seconds: props.seconds || defaultTime,
+      timer: null
+    };
+    this.state = this.initialState;
   }
 
+  countDown = () => {
+    this.setState(
+      ({ seconds }) => ({ seconds: seconds - 1 }),
+      () => this.state.seconds < 0 && this.handleStop()
+    );
+  };
+
+  handleStart = cb => {
+    const callback = typeof cb === "function" ? cb : () => {};
+    if (!this.state.timer && this.state.seconds > 0) {
+      this.setState({ timer: setInterval(this.countDown, 1000) }, callback);
+    }
+  };
+
+  handleStop = cb => {
+    const callback = typeof cb === "function" ? cb : () => {};
+    clearInterval(this.state.timer);
+    this.setState(this.initialState, callback);
+  };
+
+  handleReset = () => {
+    const callback = typeof cb === "function" ? cb : () => {};
+    this.handleStop(this.handleStart);
+  };
+
   render() {
+    const { children } = this.props;
     return (
-      <div className="motivation">
-        {this.text[Math.floor(Math.random() * this.text.length)]}
+      <div className="container">
+        <Time.Provider
+          value={{
+            seconds: this.state.seconds,
+            onStart: this.handleStart,
+            onStop: this.handleStop,
+            onReset: this.handleReset,
+            isCountingDown: this.state.timer
+          }}
+        >
+          {children}
+        </Time.Provider>
       </div>
     );
   }
 }
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      seconds: 60,
-      timer: null
-    };
-  }
-
-  startTimer = () => {
-    console.log(this.timer, this.state.seconds);
-    if (!this.state.timer && this.state.seconds > 0) {
-      this.setState({ timer: setInterval(this.countDown, 1000) });
-    }
-  };
-
-  resetTimer = () => {
-    this.clearTimer(this.startTimer);
-  };
-
-  clearTimer = cb => {
-    clearInterval(this.state.timer);
-    this.setState(
-      { seconds: 60, timer: null },
-      typeof cb === "function" ? cb : () => null
-    );
-  };
-
-  countDown = () => {
-    console.log(this.state.seconds);
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.seconds - 1;
-    this.setState({
-      seconds: seconds
-    });
-
-    // Check if we're at zero.
-    if (seconds === 0) {
-      this.clearTimer();
-    }
-  };
-
+class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <Countdown>
+        <TimeView />
         <div className="button-container">
-          <button
-            onClick={!this.state.timer ? this.startTimer : this.resetTimer}
-          >
-            {this.state.timer ? "Reset" : "Start"}
-          </button>
-          {this.state.timer ? (
-            <button onClick={this.clearTimer}>Stop</button>
-          ) : null}
+          <StartButton />
+          <StopButton />
         </div>
-        {this.state.seconds < 10 ? (
-          <div className="motivation final">Almost there!</div>
-        ) : null}
-        <Motivation />
-        {this.state.timer ? <div className="time">
-          {this.state.seconds < 10 ? "0" : ""}
-          {this.state.seconds}s
-        </div> : null }
-      </React.Fragment>
+        <div className="button-container">
+          <ResetButton />
+        </div>
+      </Countdown>
     );
   }
 }
